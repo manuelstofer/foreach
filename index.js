2dd53b6950f02ec3b5538ce0264e11f25516a3ab
Manuel Stofer <mst@rtp.ch>
2013-03-22 10:23:36 +0100

    Adds back .hasOwnProperty

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ module.exports = function (obj, fn, ctx) {
     if (obj == null) return;
     var l = obj.length;
     if (l === +l) {
-        for (var k = 0; k < l; k++) {
-            fn.call(ctx, obj[k], k);
+        for (var i = 0; i < l; i++) {
+            fn.call(ctx, obj[i], i);
         }
     } else {
         for (var k in obj) {
-            fn.call(ctx, obj[k], k);
+            if (Object.prototype.hasOwnProperty.call(obj,k)){
+                fn.call(ctx, obj[k], k);
+            }
         }
     }
 };
